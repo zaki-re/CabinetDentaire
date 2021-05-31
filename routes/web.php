8af1e558f0eae3315934627766daa07f52ec582c
Zakaria REZZOUG <zrezzoug@amana.local>
2021-05-31 12:00:18 +0100

    fixing some issus

--- routes/web.php
+++ routes/web.php
@@ -28,22 +28,30 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/getMalade', [App\Http\Controllers\Malade\MaladeController::class, 'getMalade']);
 
     Route::get('/consulter_profile/{id_malade}', [App\Http\Controllers\Malade\MaladeController::class, 'showProfile'])->name('profile.name');
+
+
     Route::get('/antecedants', [App\Http\Controllers\Medecin\AntecedantsController::class, 'antecedantsShow'])->name('show.antecedants');
     Route::get('/get_antecedants', [App\Http\Controllers\Medecin\AntecedantsController::class, 'getAntecedants'])->name('get.antecedants');
     Route::post('/ajouter_antecedant', [App\Http\Controllers\Medecin\AntecedantsController::class, 'createAntecedants'])->name('add.antecedants');
     Route::get('/delete_antecedants/{id_antecedants}', [App\Http\Controllers\Medecin\AntecedantsController::class, 'removeAntecedants'])->name('delete.antecedants');
+
+
     Route::get('/delete_malade/{id_antecedants}', [App\Http\Controllers\Malade\MaladeController::class, 'destroy'])->name('delete.malade');
     Route::get('/nombre_de_malade', [App\Http\Controllers\Malade\MaladeController::class, 'nbrDeMalade'])->name('nombre.malade');
     Route::get('/malade_antecedants/{id_malade}', [App\Http\Controllers\Medecin\AntecedantsController::class, 'maladeAntecedants'])->name('malade.antecedants');
+
+
+
     Route::get('/type_consultations', [App\Http\Controllers\Medecin\TypeConsultationController::class, 'index'])->name('show.consultation');
     Route::get('/get_type_consultations', [App\Http\Controllers\Medecin\TypeConsultationController::class, 'show'])->name('display.type.consultation');
     Route::get('/delete_type_consultation/{id_consultation}', [App\Http\Controllers\Medecin\TypeConsultationController::class, 'destroy'])->name('delete.type.consultation');
     Route::post('/ajouter_type_consultation', [App\Http\Controllers\Medecin\TypeConsultationController::class, 'store'])->name('ajotuer.type.consultation');
-    Route::post('/update_patient/{id}', [App\Http\Controllers\Malade\MaladeController::class, 'update'])->name('ajotuer.consultation');
-    Route::get('/getDents', [App\Http\Controllers\Medecin\DentController::class, 'show'])->name('get.dents');
+
+
+    Route::post('/update_patient/{id}', [App\Http\Controllers\Malade\MaladeController::class, 'update'])->name('update.patient');
     Route::get('/getDents', [App\Http\Controllers\Medecin\DentController::class, 'show'])->name('get.dents');
     Route::post('/ajotuer_consultation', [App\Http\Controllers\Medecin\ConsultationController::class, 'store'])->name('ajotuer.consultation');
-    Route::get('/get_consultation/{id_malade}', [App\Http\Controllers\Medecin\ConsultationController::class, 'show'])->name('ajotuer.consultation');
+    Route::get('/get_consultation/{id_malade}', [App\Http\Controllers\Medecin\ConsultationController::class, 'show'])->name('get.malade.consultation');
     Route::get('/get_dents_consultation/{id_malade}', [App\Http\Controllers\Medecin\ConsultationController::class, 'getDentsConsultation'])->name('get.dents.consultation');
 
 
